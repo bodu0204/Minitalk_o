9f8b0ad9475e55f31de7cb02196b3820a257bac2
呂明 <ryoakira@ryoakiranoMacBook-Pro.local>
2022-03-27 10:55:13 +0900

    ...

---
+++ otherscode/intra-uuid-19cef627-1777-40b5-a1f1-02105b257513-4032302-cshono/ft_atoi.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cshono <cshono@student.42.tokyo>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/22 19:26:32 by cshono            #+#    #+#             */
+/*   Updated: 2022/03/25 00:23:16 by cshono           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	size_t	i;
+	int		sign;
+	int		ret;
+
+	i = 0;
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
+		|| str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	else
+		sign = 1;
+	if (sign == -1 || str[i] == '+')
+		i++;
+	ret = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+		ret = (ret * 10) + (str[i++] - '0');
+	return (ret * sign);
+}
